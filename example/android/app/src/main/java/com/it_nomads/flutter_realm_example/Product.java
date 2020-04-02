7080dc90fb4f8ec7a20e3737d73119eeafc3c7cb
Minsu Lee <amond@amond.net>
2020-04-02 11:55:03 +0900

    build(android): realm 6.0.2

--- example/android/app/src/main/java/com/it_nomads/flutter_realm_example/Product.java
+++ example/android/app/src/main/java/com/it_nomads/flutter_realm_example/Product.java
@@ -3,9 +3,10 @@ package com.it_nomads.flutter_realm_example;
 import io.realm.RealmObject;
 import io.realm.annotations.Required;
 import io.realm.annotations.PrimaryKey;
-
+import java.util.*;
 public class Product extends RealmObject  {
     @PrimaryKey
     private String uuid;
     private String title;
+    private Date createdAt;
 }
